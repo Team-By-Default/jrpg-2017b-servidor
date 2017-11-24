af45a9ffacb5555ea23ed922419a90b4096a8a36
Valeria Silvestri <valeria.ayelen@gmail.com>
2017-11-24 14:14:00 -0300

    Creo que esto soluciona los errores despues del registro

--- src/main/java/comandos/Salir.java
+++ src/main/java/comandos/Salir.java
@@ -21,7 +21,7 @@ public class Salir extends ComandosServer {
 		
 		
 		// Lo elimino de los clientes conectados
-		Servidor.getClientesConectados().remove(this);
+		Servidor.getClientesConectados().remove(escuchaCliente);
 		Paquete paquete = (Paquete) gson.fromJson(cadenaLeida, Paquete.class);
 		// Indico que se desconecto
 		Servidor.log.append(paquete.getIp() + " se ha desconectado." + System.lineSeparator());
